56d59d606af205686f73b67cfbeb17281fbdfca1
NayimWd <nayim.wd@gmail.com>
2025-05-16 05:28:45 +0600

    feat: add copy button with tooltip created shared functionality

--- src/component/common/card/BlogCard.tsx
+++ src/component/common/card/BlogCard.tsx
@@ -4,38 +4,50 @@ import Buttons from "../Buttons";
 import { Card } from "./Card";
 import { Link } from "react-router-dom";
 import { formatDate } from "../../../utils/timeFormat";
+import CopyButton from "../../ui/CopyButton";
+import { useCurrentUrl } from "../../../utils/Url";
 
 
 
 interface BlogCardProps {
-    _id: string;
-    title: string;
-    photo: string | string[];
-    author: string;
-    tags: string;
-    createdAt: string;
+  _id: string;
+  title: string;
+  photo: string | string[];
+  author: string;
+  tags: string;
+  createdAt: string;
 }
 
-const BlogCard = ({_id, title, photo, author, tags, createdAt}: BlogCardProps) => {
+const BlogCard = ({ _id, title, photo, author, tags, createdAt }: BlogCardProps) => {
+
+  const currentUrl = useCurrentUrl();
+  const urlPath = `${currentUrl}/details/${_id}`;
+  const handleShare = () => {
+    navigator.clipboard.writeText(urlPath);
+  }
+
   return (
-    <Card size="md" variant="Blog" className="text-font overflow-hidden flex flex-col gap-4 ">
+    <Card size="md" variant="Blog" className="relative text-font overflow-hidden flex flex-col gap-4 ">
       <div>
-        <img src={photo[0]} alt="Blog" className="w-full h-56 object-cover rounded" loading="lazy"/>
+        <img src={photo[0]} alt="Blog" className="w-full h-56 object-cover rounded" loading="lazy" />
       </div>
       <div>
         <div className="flex justify-between items-center mb-5">
           <p className={`${fontStyle.cardDesc} text-secondary`}>{formatDate(createdAt)}</p>
           <Link to={`/dashboard/blogs/details/${_id}`}>
-          <Buttons variant="primary" size="sm" className="rounded-sm" iconRight={<MoveRight size={8}/>}> Details </Buttons>
+            <Buttons variant="primary" size="sm" className="rounded-sm" iconRight={<MoveRight size={8} />}> Details </Buttons>
           </Link>
-      </div>
-      <h3 className={`${fontStyle.cardTitle} text-font`}> {title.slice(0, 130)}... </h3>
-      <div className="flex justify-between flex-wrap items-center w-full mt-5 text-subtext">
+        </div>
+        <h3 className={`${fontStyle.cardTitle} text-font`}> {title.slice(0, 130)}... </h3>
+        <div className="flex justify-between flex-wrap items-center w-full mt-5 text-subtext">
           <p className={`${fontStyle.secondaryText} `}>{author}</p>
           <p className={fontStyle.secondaryText}>{tags}</p>
+        </div>
       </div>
+      <div onClick={handleShare} className="absolute top-6 right-6 z-50">
+        <CopyButton textCopy="Share" />
       </div>
-      </Card>
+    </Card>
   )
 }
 
--- src/pages/Blogs/BlogDetails.tsx
+++ src/pages/Blogs/BlogDetails.tsx
@@ -1,23 +1,17 @@
 import { useParams } from "react-router-dom"
+import Banner from "../../component/common/banner/Banner";
+
 
 const BlogDetails = () => {
     const {blogId} = useParams();
+
+    
 
     console.log(blogId)
 
   return (
     <div className="w-full">
-      <div className=" shadow flex items-center h-48 rounded-md w-full bg-bg glassBg">
-					<div className="px-2 flex flex-wrap justify-start items-center gap-2 leading-7 tracking-wider sm:text-lg font-bold">
-						{" "}
-						<span className="text-primary"> </span>{" "}
-						<span className="text-primary">
-							{" "}
-							Product
-							<span className="text-2xl text-primary"> &#62;</span> abc
-						</span>
-					</div>
-				</div>
+      <Banner pageText="Blog Details" navText="Blog"/>
       <p className="text-font">{blogId}</p>
       BlogDetails
       </div>
