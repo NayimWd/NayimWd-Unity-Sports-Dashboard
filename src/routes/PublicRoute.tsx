9f6538ce63e65eeb7ea2257abfacd737f8c1a516
NayimWd <nayim.wd@gmail.com>
2025-05-25 09:07:26 +0600

    feat: add PublicRoute and RedirectRoute components for improved routing logic, update router to utilize new routes

---
+++ src/routes/PublicRoute.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react"
+import { useSelector } from "react-redux";
+import { RootState } from "../app/store/store";
+import { Navigate } from "react-router-dom";
+
+interface PubLicRouteProps {
+    children: ReactNode;
+}
+
+const PublicRoute = ({ children }: PubLicRouteProps) => {
+    // access user from redux store 
+    const { isAuthenticated, user } = useSelector((state: RootState) => state.auth);
+
+    // redirect if user loggedin 
+    if (isAuthenticated && user?._id) {
+        return <Navigate to="/dashboard" />
+    }
+
+    return <> {children} </>
+}
+
+export default PublicRoute;
