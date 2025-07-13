0027b4487c5716f5f5cffa58496b7aeee5f2d5af
NayimWd <nayim.wd@gmail.com>
2025-07-13 22:41:54 +0600

    feat: add updateBlogPhotoSchema for validating blog photo uploads; implement handleSubmit in UpdateBlogPhoto for photo update functionality

--- src/pages/Blogs/UpdateBlogPhoto.tsx
+++ src/pages/Blogs/UpdateBlogPhoto.tsx
@@ -1,20 +1,77 @@
-import { useParams } from "react-router-dom"
+import { z } from "zod";
+import { useNavigate, useParams } from "react-router-dom";
 import FormContainer from "../../component/common/Form/FormContainer";
 import PhotoArrayInput from "../../component/common/input/PhotoArrayInput";
+import { updateBlogPhotoSchema } from "../../utils/Schema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ErrorToast, LoadingToast, SuccessToast } from "../../utils/toastUtils";
+import { useBlogDetailsQuery, useUpdateBlogPhotoMutation } from "../../features/blog/blogApi";
+import Buttons from "../../component/common/Buttons";
+import { Edit2 } from "lucide-react";
+import toast from "react-hot-toast";
+
+type updateBologPhotoType = z.infer<typeof updateBlogPhotoSchema>;
 
 const UpdateBlogPhoto = () => {
   // blog id
   const { blogId } = useParams();
 
-  
+  // get blog photo from blog details
+  const { data: blog } = useBlogDetailsQuery(blogId);
+
+  // update blog photo, api slice 
+  const [updateBlogPhoto, { isLoading }] = useUpdateBlogPhotoMutation();
+
+  //  navigate
+  const navigate = useNavigate();
+
+  // form method
+  const methods = useForm<updateBologPhotoType>({
+    resolver: zodResolver(updateBlogPhotoSchema),
+    mode: "onSubmit"
+  });
+
+  // function 
+  const handleSubmit = async (data: updateBologPhotoType) => {
+    const loadingId = LoadingToast({ msg: "Updating Blog Photo..." });
+    try {
+      const formData = new FormData();
+      if (data.photo && Array.isArray(data.photo) && data.photo.length > 0) {
+        formData.append("photo", data.photo[0]);
+      };
+      // update photo
+      await updateBlogPhoto({ blogId, data: formData }).unwrap();
+      toast.dismiss(loadingId);
+      SuccessToast({ msg: "Blog photo updated Successfully" })
+      methods.reset();
+      navigate("/dashboard/blogs")
+
+    } catch (error) {
+      toast.dismiss(loadingId);
+      ErrorToast({ msg: "Update Blog Photo Failed!" })
+    }
+  };
 
   return (
     <div className="w-full">
       <h1 className=" text-center text-2xl sm:text-3xl md:text-4xl mt-5 font-semibold text-font font-merriweather">Edit Blogs Photo</h1>
       <div className="paddingX w-full max-w-5xl mx-auto bg-surface  py-12 px-6 rounded shadow-sm">
-          <FormContainer >
-            <PhotoArrayInput label="Image" name="photo" className="" />
-          </FormContainer>
+        <div>
+          <img src={Array.isArray(blog?.photo) ? blog?.photo[0] : blog?.photo} alt="blog photo" loading="lazy"
+            className="w-full h-[420px] object-scale-down object-center transition-transform duration-300 hover:scale-105"
+          />
+        </div>
+        <FormContainer
+          methods={methods}
+          onSubmit={handleSubmit}
+          className=" w-full space-y-6 paddingX"
+        >
+          <PhotoArrayInput label="Image" name="photo" />
+          <Buttons iconRight={<Edit2 size={16} />} disabled={isLoading}>
+            Update Photo
+          </Buttons>
+        </FormContainer>
       </div>
     </div>
   )
