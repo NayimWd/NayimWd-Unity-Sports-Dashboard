295d67060781efd37420dc8928009dffce002f83
NayimWd <nayim.wd@gmail.com>
2025-07-16 14:51:54 +0600

    feat: update Account component text and add account routes for navigation

--- src/routes/dashboardRoute.tsx
+++ src/routes/dashboardRoute.tsx
@@ -1,8 +1,10 @@
 import { RouteObject } from "react-router-dom";
 import { pointTableRoutes } from "./pointTable";
 import { blogRoutes } from "./blogRoute";
+import { accountRoutes } from "./accountRoute";
 
 export const dashboardRoutes: RouteObject[] = [
     ...pointTableRoutes,
     ...blogRoutes,
+    ...accountRoutes,
 ]
