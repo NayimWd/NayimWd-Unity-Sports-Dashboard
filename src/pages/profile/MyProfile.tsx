046a6e62ca04796bd7351f8116be33d37360581b
NayimWd <nayim.wd@gmail.com>
2025-11-01 08:49:35 +0600

    feat: add BackButton component to MyProfile page for improved navigation; refactor BackButton implementation for better functionality

--- src/pages/profile/MyProfile.tsx
+++ src/pages/profile/MyProfile.tsx
@@ -2,11 +2,13 @@ import { User } from "lucide-react";
 import Badge from "../../component/ui/Badge";
 import SectionLayout from "../../component/layout/SectionLayout";
 import PageLayout from "../../component/layout/PageLayout";
+import BackButton from "../../utils/BackButton";
 
 const MyProfile = () => {
 
   return (
     <PageLayout>
+      <BackButton link="/dashboard">Go Home</BackButton>
       <div className="flex items-center gap-3 flex-wrap">
       <Badge icon={<User size={12} />} variant="default"> default</Badge>
       <Badge variant="success">success</Badge>
