4fbdd4b64bd625ddd3f67f6602ebc3ddf990479c
NayimWd <nayim.wd@gmail.com>
2025-11-19 16:30:09 +0600

    feat: add team API endpoints and types; update ApiResponse to make success optional

---
+++ src/utils/types/teamType.ts
@@ -0,0 +1,18 @@
+export interface ITeam {
+  _id: string;
+  playerCount: number;
+  teamName: string;
+  teamLogo: string;
+}
+
+export interface ITeamPagination {
+  currentPage: number;
+  totalPages: number;
+  totalTeams: number;
+}
+
+export interface IAllTeams {
+  totalTeams: number;
+  teams: ITeam[];
+  pagination: ITeamPagination;
+}
