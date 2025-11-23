a30f473e984e7a138a0f9ba43a1d728464356e27
NayimWd <nayim.wd@gmail.com>
2025-11-24 01:50:50 +0600

    feat: add team player details with proper types

--- src/utils/types/playerType.ts
+++ src/utils/types/playerType.ts
@@ -7,13 +7,44 @@ export interface IPlayer {
   isCaptain: boolean;
 }
 
-
 export interface IManager {
-    _id: string,
-    name: string,
-    photo: string,
-    email: string,
+  _id: string;
+  name: string;
+  photo: string;
+  email: string;
 }
 
+// for player details response type
+interface ITeamMini {
+  _id: string;
+  teamName: string;
+  teamLogo: string;
+}
 
+interface IPlayerLocal {
+  _id: string;
+  name: string;
+  photo: string;
+  role: string;
+}
 
+interface IPlayerMini {
+  isCaptain: boolean;
+  status: string;
+  playerId: IPlayerLocal;
+  teamId: ITeamMini;
+}
+
+interface IPlayerProfile {
+  _id: string;
+  player_role: string;
+  batingStyle: string;
+  bowlingArm: string;
+  bowlingStyle: string;
+  DateOfBirth: string;
+}
+
+export interface IPlayerDetails {
+  player: IPlayerMini;
+  playerProfile: IPlayerProfile;
+}
