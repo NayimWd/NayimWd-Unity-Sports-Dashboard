22813d37e73644347073dd9afa5fa102a19d6b54
NayimWd <nayim.wd@gmail.com>
2025-11-24 14:55:55 +0600

    update: set tag types for venue list and single venue, improve venues error and empty case ui

--- src/features/venue/venueApi.ts
+++ src/features/venue/venueApi.ts
@@ -3,22 +3,38 @@ import { IVenue, IVenueDetails } from "../../utils/types/venueType";
 import { apiSlice } from "../api/apiSlice";
 
 const venueApi = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        getVenue: builder.query<ApiResponse<IVenue>, void>({
-            query: () => ({
-                url: `venue/all`,
-                method: "GET"
-            }),
-            transformResponse: (response: ApiResponse<IVenue>) => response,
-        }),
-        getVenueDetails: builder.query<ApiResponse<IVenueDetails>, string>({
-            query: (venueId)=> ({
-                url: `venue/details/${venueId}`,
-                method: "GET"
-            }),
-            transformResponse: (response: ApiResponse<IVenueDetails>) => response,
-        })
+  endpoints: (builder) => ({
+    getVenue: builder.query<ApiResponse<IVenue>, void>({
+      query: () => ({
+        url: `venue/all`,
+        method: "GET",
+      }),
+      transformResponse: (response: ApiResponse<IVenue>) => response,
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.data.venues.map(({ _id }) => ({
+                type: "Venue" as const,
+                id: _id,
+              })),
+              { type: "Venue", id: "LIST" },
+            ]
+          : [{ type: "Venue", id: "LIST" }],
     }),
+    getVenueDetails: builder.query<ApiResponse<IVenueDetails>, string>({
+      query: (venueId) => ({
+        url: `venue/details/${venueId}`,
+        method: "GET",
+      }),
+      transformResponse: (response: ApiResponse<IVenueDetails>) => response,
+      providesTags: (_result, _error, venueId) => [
+        {
+          type: "Venue",
+          id: venueId,
+        },
+      ],
+    }),
+  }),
 });
 
-export const {useGetVenueQuery, useGetVenueDetailsQuery} = venueApi;
+export const { useGetVenueQuery, useGetVenueDetailsQuery } = venueApi;
--- src/pages/venue/Venues.tsx
+++ src/pages/venue/Venues.tsx
@@ -1,5 +1,6 @@
 import VenueCard from "../../component/common/card/VenueCard";
 import PageLayout from "../../component/layout/PageLayout"
+import EmptyData from "../../component/ui/EmptyData";
 import { useGetVenueQuery } from "../../features/venue/venueApi"
 import { useGoBack } from "../../hooks/useGoBack";
 import BackButton from "../../utils/BackButton";
@@ -10,10 +11,11 @@ const Venues = () => {
   const { data, isError, refetch } = useGetVenueQuery();
 
 
+  let content = null;
 
   // handle error 
-  if (isError || !data?.data.venues?.length)
-    return (
+  if (isError)
+    content = (
       <PageLayout>
         <BackButton onClick={goBack}>Back</BackButton>
         <div className="mt-10 flex flex-col items-center gap-3 text-center">
@@ -28,18 +30,13 @@ const Venues = () => {
           </button>
         </div>
       </PageLayout>
-    );
-
-  return (
-    <PageLayout>
-      <BackButton onClick={goBack}>Back</BackButton>
-      <div className="flex justify-between items-center mt-8">
-        <h1 className={`${fontStyle.SectionHeading} text-font`}>
-          Venues
-        </h1>
-        <p className="text-lg text-muted">Total Venue: {data?.data.total}</p>
-      </div>
-      <div className="w-full mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center ">
+    ); 
+    if(!data?.data.venues?.length){
+      content = (
+        <EmptyData message="Venue Not found!"/>
+      )
+    } else {
+      content = <div className="w-full mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center ">
         {data?.data.venues.map((venue) => (
           <VenueCard
             key={venue._id}
@@ -47,6 +44,22 @@ const Venues = () => {
           />
         ))}
       </div>
+    }
+
+
+  return (
+    <PageLayout>
+      <BackButton onClick={goBack}>Back</BackButton>
+        <h1 className={`${fontStyle.pageTitle} text-font text-center`}>All Venue</h1>
+      <div className="flex justify-between items-center mt-8">
+        <h2 className={`${fontStyle.SectionHeading} text-font`}>
+          Venues
+        </h2>
+        <p className="text-lg text-muted">Total Venue: {data?.data.total ? data?.data.total : 0}</p>
+      </div>
+      {
+        content
+      }
     </PageLayout>
   )
 }
