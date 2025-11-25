a954b55bb96be466c92906a7aed1089e4e59bcfc
NayimWd <nayim.wd@gmail.com>
2025-11-26 04:56:32 +0600

    feat: tournaments page created with cards and apiSlices

--- src/features/tournament/tournamentApi.ts
+++ src/features/tournament/tournamentApi.ts
@@ -1,3 +1,4 @@
+import { ITournaments } from "../../utils/types/tournamentTypes";
 import { ApiResponse, Tournament } from "../../utils/types/types";
 import { apiSlice } from "../api/apiSlice";
 
@@ -6,7 +7,26 @@ export const tournamentApi = apiSlice.injectEndpoints({
     latestTournament: builder.query<ApiResponse<Tournament>, void>({
       query: () => `tournament/latest`,
     }),
+    getAllTournament: builder.query<ApiResponse<ITournaments>, string | void>({
+      query: (status) => {
+        const param = status ? `status=${status}` : "";
+
+        return { url: `tournament/status?${param}`, method: "GET" };
+      },
+      transformResponse: (response: ApiResponse<ITournaments>) => response,
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.data.tournaments.map((t) => ({
+                type: "Tournament" as const,
+                id: t._id,
+              })),
+              { type: "Tournament", id: "LIST" },
+            ]
+          : [{ type: "Tournament", id: "LIST" }],
+    }),
   }),
 });
 
-export const { useLatestTournamentQuery } = tournamentApi;
+export const { useLatestTournamentQuery, useGetAllTournamentQuery } =
+  tournamentApi;
