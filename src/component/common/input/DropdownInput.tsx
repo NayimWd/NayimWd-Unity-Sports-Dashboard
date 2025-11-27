b1cec726d8adb093acebc8cdf4f45001fa79f462
NayimWd <nayim.wd@gmail.com>
2025-11-27 07:55:15 +0600

    fix: create tournament and details page updated

--- src/component/common/input/DropdownInput.tsx
+++ src/component/common/input/DropdownInput.tsx
@@ -1,74 +1,73 @@
+import { Controller, useFormContext } from "react-hook-form";
 import { BadgeAlert, ChevronDown } from "lucide-react";
-import React from "react";
-import { useFormContext } from "react-hook-form";
-
-interface DropdownOptions {
-  label: string;
-  value: string | number;
-}
 
 interface DropdownInputProps {
   name: string;
   label: string;
   placeholder?: string;
-  options: DropdownOptions[];
-  icon?: React.ReactNode;
+  options: { label: string | number; value: string | number }[];
 }
 
-const DropdownInput = ({
-  name,
-  label,
-  placeholder = "Select an option",
-  options,
-  icon,
-}: DropdownInputProps) => {
-  const { register, formState } = useFormContext();
-  const { errors } = formState;
-  const error = errors[name]?.message as string | undefined;
+const DropdownInput = ({ name, label, placeholder, options }: DropdownInputProps) => {
+  const { control, formState } = useFormContext();
+  const error = formState.errors[name]?.message as string | undefined;
 
   const variant: "primary" | "error" = error ? "error" : "primary";
 
+
   return (
-    <div className="">
-      <label className="block font-medium text-font mb-1" htmlFor={name}>
-        {label}
-      </label>
-      <div className="relative">
-        <select
-          id={name}
-          {...register(name)}
-          className={`w-full cursor-pointer h-10 px-3 py-2 text-font bg-surface rounded-md appearance-none text-sm transition-all duration-150 ease-in-out focus:outline-none border focus:ring-2 p-1 
+    <div className="space-y-1">
+      <label className="text-font font-medium">{label}</label>
+
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <div className="relative">
+            <select
+              className={`w-full cursor-pointer h-10 px-3 py-2 text-font bg-surface rounded-md appearance-none text-sm transition-all duration-150 ease-in-out focus:outline-none border focus:ring-2 p-1 
     ${variant === "error"
-              ? "border-toastErrorText focus:ring-toastErrorText"
-              : "border-inputBorder focus:ring-primary"
-            }    
+                  ? "border-toastErrorText focus:ring-toastErrorText"
+                  : "border-inputBorder focus:ring-primary"
+                }    
   `}
-        >
-          <option value="" disabled hidden className="text-muted">
-            {placeholder}
-          </option>
-          {options.map((option) => (
-            <option
-              className="text-font bg-bg"
-              key={option.value}
-              value={option.value}
+              value={field.value ?? ""}
+              onChange={(e) => {
+                const val = e.target.value;
+
+                // Convert number-like values into real numbers
+                if (!isNaN(Number(val))) {
+                  field.onChange(Number(val));
+                } else {
+                  field.onChange(val);
+                }
+              }}
             >
-              {option.label}
-            </option>
-          ))}
-        </select>
-        {/* optional icon */}
-        {icon && (
-          <div className="absolute left-3 top-1/2 -translate-y-1/2">{icon}</div>
+              {placeholder && (
+                <option value="" disabled hidden className="text-muted">
+                  {placeholder}
+                </option>
+              )}
+              {options.map((op) => (
+                <option className="text-font bg-bg" key={op.value} value={op.value}>
+                  {op.label}
+                </option>
+              ))}
+            </select>
+
+            <ChevronDown
+              size={16}
+              className="absolute right-3 top-1/2 -translate-y-1/2 text-muted pointer-events-none"
+            />
+          </div>
         )}
-        {/*Dropdown arrow*/}
-        <ChevronDown
-          size={16}
-          className="absolute right-3 top-1/2 -translate-y-1/2 pointer-events-none text-muted"
-        />
-      </div>
-      {error && <p className="errorText flex items-center justify-center gap-1"> <BadgeAlert size={14} /> {error}</p>
-      }
+      />
+
+      {error && (
+        <p className="errorText flex items-center gap-1 text-xs">
+          <BadgeAlert size={14} /> {error}
+        </p>
+      )}
     </div>
   );
 };
