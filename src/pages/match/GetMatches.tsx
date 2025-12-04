f150a0ebb00c5cf04503f3fa72325ea0f1c48b0c
NayimWd <nayim.wd@gmail.com>
2025-12-04 13:29:12 +0600

    update: created types for get all match, and apislice created

--- src/pages/match/GetMatches.tsx
+++ src/pages/match/GetMatches.tsx
@@ -1,6 +1,26 @@
+import PageLayout from "../../component/layout/PageLayout"
+import { useGetMatchQuery } from "../../features/match/matchApi";
+import { useLatestTournamentQuery } from "../../features/tournament/tournamentApi"
+import { useGoBack } from "../../hooks/useGoBack";
+import BackButton from "../../utils/BackButton";
+
 const GetMatches = () => {
+  const goBack = useGoBack();
+
+// get letest tournament id based on point table
+ const {data} = useLatestTournamentQuery();
+
+ const tournamentId = data?.data._id;
+
+ // fetch match
+ const {data: matches} =  useGetMatchQuery({tournamentId}, {skip: !tournamentId});
+
+console.log(matches)
+
   return (
-    <div>GetMatches</div>
+    <PageLayout>
+      <BackButton onClick={goBack}>Back</BackButton>
+    </PageLayout>
   )
 }
 
