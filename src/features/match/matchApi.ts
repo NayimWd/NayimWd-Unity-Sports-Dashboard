ed48fe2ec694c78877e783d919713afff300b953
NayimWd <nayim.wd@gmail.com>
2026-01-27 04:57:25 +0600

    feat(match): created match card and match card page

--- src/features/match/matchApi.ts
+++ src/features/match/matchApi.ts
@@ -1,12 +1,15 @@
+import { MatchListData, TournamentMatch } from "../../utils/types/matchTypes";
+import { ApiResponse } from "../../utils/types/types";
 import { apiSlice } from "../api/apiSlice";
 
 const matchApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
-        getMatch: builder.query({
+        getMatch: builder.query<MatchListData, any>({
             query: ({tournamentId}) => ({
                 url: `match/all/${tournamentId}`,
                 method: "GET"
             }),
+            transformResponse: (response: ApiResponse<MatchListData>) => response.data 
         }),
     }),
 });
